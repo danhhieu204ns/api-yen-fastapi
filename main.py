49c3d0370820a118474de8e58a8e59c81896d550
Hieu <danhhieu123ns@gmail.com>
2024-12-09 18:21:16 +0700

    refactor: reorganize router imports and include new permission and user routers

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from configs.database import Base, engine
 from configs.conf import settings
 from role.routers import role
-from user_info.routers import user_info
-from user_account.routers import user_account
+from permission.routers import permission
+from role_permission.routers import role_permission
+from user.routers import user
+from auth_credential.routers import auth_credential
 from user_role.routers import user_role
 from authen.routers import authen
 from author.routers import author
@@ -15,7 +16,7 @@ from book.routers import book
 from book_copy.routers import book_copy
 from bookshelf.routers import bookshelf
 from borrow.routers import borrow
-
+import uvicorn
 
 
 Base.metadata.create_all(bind=engine)
@@ -39,17 +40,20 @@ app.add_middleware(
 async def root():
     return {"message": "Hello World"}
 
+
 app.router.include_router(role.router)
-app.router.include_router(user_info.router)
-app.router.include_router(user_account.router)
+app.router.include_router(permission.router)
+app.router.include_router(role_permission.router)
+app.router.include_router(user.router)
+app.router.include_router(auth_credential.router)
 app.router.include_router(user_role.router)
 app.router.include_router(authen.router)
 app.router.include_router(author.router)
 app.router.include_router(category.router)
 app.router.include_router(publisher.router)
 app.router.include_router(book.router)
-app.router.include_router(book_copy.router)
 app.router.include_router(bookshelf.router)
+app.router.include_router(book_copy.router)
 app.router.include_router(borrow.router)
 
 
