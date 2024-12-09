71d2fd940aebd2bb1673e5bb98297eed2b958648
Hieu <danhhieu123ns@gmail.com>
2024-12-09 18:20:57 +0700

    feat: update authentication module to use User model and improve token handling

--- configs/authentication.py
+++ configs/authentication.py
@@ -7,7 +7,7 @@ from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from authen.schemas.authen import Tokendata
 from configs.database import get_db
-from user_info.models.user_info import UserInfo
+from user.models.user import User
 from .conf import settings
 
 
@@ -35,16 +35,18 @@ def create_access_token(data: dict):
     to_encode.update({"exp": expire})
 
     encode_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encode_jwt
+
+    return encode_jwt, expire
 
 
 def verify_access_token(token: str, credentials_exception):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
-        id: str = payload.get("user_id")
-        if not id:
+        user_id: str = payload.get("user_id")
+        if not user_id:
             raise credentials_exception
-        token_data = Tokendata(id=id)
+        token_data = Tokendata(user_id=user_id)
+
     except JWTError:
         raise credentials_exception
     
@@ -52,11 +54,13 @@ def verify_access_token(token: str, credentials_exception):
 
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
-    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                                          detail="Could not validate credentials", 
-                                          headers={"WWW-Authenticate": "Bearer"})
-    token = verify_access_token(token, credentials_exception)
-    user = db.query(UserInfo).filter(UserInfo.id == token.id).first()
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials", 
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    token = verify_access_token(token, credentials_exception) 
+    user = db.query(User).filter(User.id == token.user_id).first()
     return user
 
 
@@ -64,11 +68,11 @@ def validate_pwd(password):
     if len(password) < 8:
         raise HTTPException(
             status_code=status.HTTP_406_NOT_ACCEPTABLE,
-            detail="Mật khẩu phải có ít nhất 8 kí tự."
+            detail="Mật khẩu phải có ít nhất 8 kí tự"
         )
     
     if not re.search(r'[A-Z]', password):
         raise HTTPException(
             status_code=status.HTTP_406_NOT_ACCEPTABLE,
-            detail="Mật khẩu phải có ít nhất 1 kí tự in hoa."
+            detail="Mật khẩu phải có ít nhất 1 kí tự in hoa"
         )
